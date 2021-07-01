ce0c75cf9666b16e47336a176f34bbfbcdc7f1dc
mizuka.wu <mizuka.wu@outlook.com>
2021-07-01 14:48:52 +0800

    feat: live2d

---
+++ Live2d.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script>
+export default {
+    name: 'Live2d'
+}
+</script>
+
+<style>
+</style>
---
+++ enhanceAppFile.js
@@ -0,0 +1,4 @@
+  import Live2d from "./Live2d.vue";
+export default ({ Vue }) => {
+  Vue.component("Live2d", Live2d);
+};
