e02441d449105a742cd3f549b78164fd1ff147f4
mizuka.wu <mizuka.wu@outlook.com>
2021-07-01 18:39:33 +0800

    feat: 支持位置

--- Live2d.vue
+++ Live2d.vue
@@ -1,24 +1,72 @@
 <template>
-  <div>{{ width }}</div>
+  <div
+    :style="containerStyle"
+    class="live2d-container"
+    v-show="isLoaded"
+  >
+    <canvas
+      height="100%"
+      id="vuepress-live2d"
+      width="100%"
+    />
+  </div>
 </template>
 
 <script>
 import live2dJSString from "./live2d";
+
+if (!window.loadlive2d) {
+  const script = document.createElement("script");
+  script.innerHTML = live2dJSString;
+  document.body.appendChild(script);
+}
+
 export default {
   props: {
-    width: {
-      type: Number,
-      default: 500
+    containerStyle: {
+      type: Object,
+      default: () => ({})
+    },
+    /**
+     * 是否mobile下显示
+     */
+    mobile: {
+      type: Boolean,
+      default: false
+    },
+    model: {
+      type: String,
+      default: "https://cdn.jsdelivr.net/gh/QiShaoXuan/live2DModel@1.0.0/live2d-widget-model-tororo/assets/tororo.model.json"
     }
   },
   data() {
+    const isMobile = !!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
     return {
-
+      isLoaded: true,
+      isMobile
     }
   },
   methods: {
+    /**
+     * 初始化live2d
+     */
     initLive2d() {
-      console.log('初始化')
+      /**
+       * 手机看配置显示不显示
+       */
+      if (this.isMobile && !this.mobile) {
+        this.isLoaded = false
+        return 0
+      }
+
+      setTimeout(() => {
+        if (window.loadlive2d) {
+          window.loadlive2d('vuepress-live2d', this.model)
+        } else {
+          console.log('未能成功加载live2d基础库')
+        }
+      }, 500)
+      
     }
   },
   mounted() {
@@ -33,4 +81,12 @@ export default {
 </script>
 
 <style>
+.live2d-container {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  z-index: 80;
+  width: 300px;
+  height: 300px;
+}
 </style>
