a52eb784e148451467d51a98157e66d932947aa7
水华 <yiwei.wu@mihoyo.com>
2021-07-01 15:56:50 +0800

    feat: 给plugin附值的方法

--- enhanceAppFile.js
+++ enhanceAppFile.js
@@ -1,4 +1,9 @@
-  import Live2d from "./Live2d.vue";
-export default ({ Vue }) => {
-  Vue.component("Live2d", Live2d);
-};
+const { resolve } = require('path')
+module.exports = function(option = {}) {
+    return `import Live2d from "${resolve(__dirname, './Live2d.vue')}";
+
+    export default ({ Vue }) => {
+      ${Object.keys(option).map(key => `Live2d.props.${key}.default = ${option[key]}`).join('\n')}  
+      Vue.component("Live2d", Live2d);
+    };`
+}
