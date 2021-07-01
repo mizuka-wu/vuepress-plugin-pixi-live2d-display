4d97f1944152d9dac1ea17f4171c71bd46cca4a0
水华 <yiwei.wu@mihoyo.com>
2021-07-01 14:54:37 +0800

    feat: 本地live2d id

--- Live2d.vue
+++ Live2d.vue
@@ -3,8 +3,22 @@
 </template>
 
 <script>
+/**
+ * @see https://www.npmjs.com/package/vue-live2d
+ */
+import isMobile from 'is-mobile'
+import vueLive2d from 'vue-live2d'
+
 export default {
-    name: 'Live2d'
+    name: 'Live2d',
+    components: {
+        // vueLive2d
+    },
+    data() {
+        return {
+            isMobile: isMobile()
+        }
+    },
 }
 </script>
 
