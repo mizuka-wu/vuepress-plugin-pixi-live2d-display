e15d2038699562034a204247fb6377a3e9965ca6
水华 <mizuka.wu@outlook.com>
2021-07-02 00:27:59 +0800

    feat: 引入模式

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ module.exports = (option) => {
         enhanceAppFiles() {
             return [
                 // 注入lib
-                resolve(__dirname, 'live2d.js'),
+                // resolve(__dirname, 'lib', 'live2dcubismcore.js'),
+                {
+                    name: "live2dcubismcore.min.js",
+                    content: `
+                    const scriptElement = document.createElement('script')
+                    scriptElement.src = "https://cdn.jsdelivr.net/gh/litstronger/live2d-​moc3@master/js/frame/live2dcubismcore.min.js"
+                    document.body.appendChild(scriptElement)
+                    `
+                },
+                resolve(__dirname, 'lib', 'pixi.js'),
+                resolve(__dirname, 'lib', 'pixi-live2d-display.js'),
                 {
                     // 注入component
                     name: 'Live2dComponent',
