4b31a5f7c55a43807397e84fdc86120601a8af13
水华 <yiwei.wu@mihoyo.com>
2021-07-01 18:39:33 +0800

    feat: 支持位置

--- enhanceAppFile.js
+++ enhanceAppFile.js
@@ -3,7 +3,20 @@ module.exports = function(option = {}) {
     return `import Live2d from "${resolve(__dirname, './Live2d.vue')}";
 
     export default ({ Vue }) => {
-      ${Object.keys(option).map(key => `Live2d.props.${key}.default = ${option[key]}`).join('\n')}  
+      ${Object.keys(option).map(key => {
+        let value = option[key]
+        switch (typeof value) {
+          case 'object': {
+            value = `() => (${JSON.stringify(value)})`
+            break
+          }
+          default: {
+            break
+          }
+        }
+
+        return `Live2d.props.${key}.default = ${value}`
+      }).join('\n')}  
       Vue.component("Live2d", Live2d);
     };`
 }
